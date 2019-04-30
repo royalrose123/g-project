9e60c536328079e2e63a5c63bb16d7186e9ada32
Patrick <patrick.tsai@cloud-interactive.com>
2019-04-30 14:25:53 +0800

    Update service api and game denormalizer

--- src/js/lib/api/Game/denormalizer.js
+++ src/js/lib/api/Game/denormalizer.js
@@ -1,43 +1,33 @@
 class Denormalizer {
-  static AnonymousClockIn ({ body: { snapshot } }) {
+  static AnonymousClockIn ({ snapshot }) {
     return {
-      body: {
-        // 格式為 [RandomString]
-        cnm: `[${Math.floor(Math.random() * 1000 * 1000 * 1000 * 1000)}]`,
-        pic: snapshot,
-      },
+      // 格式為 [RandomString]
+      cnm: `[${Math.floor(Math.random() * 1000 * 1000 * 1000 * 1000)}]`,
+      pic: snapshot,
     }
   }
 
-  static MemberClockInById ({ body: { id } }) {
+  static MemberClockInById ({ id }) {
     return {
-      body: {
-        cid: Number(id),
-      },
+      cid: Number(id),
     }
   }
 
-  static MemberClockInByCardNumber ({ body: { cardNumber } }) {
+  static MemberClockInByCardNumber ({ cardNumber }) {
     return {
-      body: {
-        cda: Number(cardNumber),
-      },
+      cda: Number(cardNumber),
     }
   }
 
-  static ClockOut ({ body: { id, playType, propPlay, averageBet, whoWin, actualWin, drop, overage } }) {
+  static ClockOut ({ id, playType, propPlay, averageBet, actualWin, drop, overage }) {
     return {
-      body: {
-        cid: Number(id),
-        ptn: Number(playType),
-        pgp: propPlay.length === 0 ? null : Number(propPlay),
-        abt: averageBet.length === 0 ? null : Number(averageBet),
-
-        // whoWin,
-        aws: actualWin.length === 0 ? null : Number(actualWin),
-        drp: drop.length === 0 ? null : Number(drop),
-        ovg: overage.length === 0 ? null : Number(overage),
-      },
+      cid: Number(id),
+      ptn: Number(playType),
+      pgp: propPlay.length === 0 ? null : Number(propPlay),
+      abt: averageBet.length === 0 ? null : Number(averageBet),
+      aws: actualWin.length === 0 ? null : Number(actualWin),
+      drp: drop.length === 0 ? null : Number(drop),
+      ovg: overage.length === 0 ? null : Number(overage),
     }
   }
 }
