e35b511501acf6c75a02c9c903a26aa1456fa265
Patrick <patrick.tsai@cloud-interactive.com>
2019-05-08 16:27:49 +0800

    Update scripts/build

--- scripts/build.js
+++ scripts/build.js
@@ -3,8 +3,7 @@
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'production';
 process.env.NODE_ENV = 'production';
-// process.env.API_URL = 'http://localhost:8080';
-process.env.API_URL = 'http://10.1.1.55:45088/v1';
+process.env.API_URL = 'http://localhost:8080';
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
