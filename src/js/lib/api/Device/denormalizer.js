11d617f7cf2dd410bfc1eafccf1d65dae0c48f2d
Hank.Lin <hank.lin@cloud-interactive.com>
2019-08-15 14:34:04 +0800

    change temp data (IP, table name)

--- src/js/lib/api/Device/denormalizer.js
+++ src/js/lib/api/Device/denormalizer.js
@@ -2,14 +2,14 @@ class Denormalizer {
   static FetchDetectionList ({ table }) {
     return {
       // TODO: 暫時先寫死，未來會從 config 帶值
-      table: 'Table-0001',
+      table: 'Table-0813',
     }
   }
 
   static FetchCameraList ({ table }) {
     return {
       // TODO: 暫時先寫死，未來會從 config 帶值
-      table: 'Table-0001',
+      table: 'Table-0813',
     }
   }
 }
