b3291af50f9d0b5f5099e2f6917e613a2e0148c8
Royal Chen <royal.chen@cloud-interactive.com>
2019-09-02 11:03:41 +0800

    Update seated coordinate constant

--- src/js/constants/SeatedCoordinate.js
+++ src/js/constants/SeatedCoordinate.js
@@ -1,10 +1,11 @@
-const CAMERA_PIXEL = () => [2592, 1944] // camera 解析度，目前為 2592 * 1944
-const CAMERA_X_MAX = () => 49 // 把畫面橫切成49等分
-const CAMERA_Y_MAX = () => 20 // 把畫面直切成20等分
-export const cameraProportion = () => ({ x: CAMERA_PIXEL[0] / CAMERA_X_MAX, y: CAMERA_PIXEL[1] / CAMERA_Y_MAX }) // 算出每一等分為多少 pixel
+const CAMERA_PIXEL = [2592, 1944] // camera 解析度，目前為 2592 * 1944
+const CAMERA_X_MAX = 49 // 把畫面橫切成49等分
+const CAMERA_Y_MAX = 20 // 把畫面直切成20等分
+
+export const cameraProportion = { x: CAMERA_PIXEL[0] / CAMERA_X_MAX, y: CAMERA_PIXEL[1] / CAMERA_Y_MAX }
 
 // 每個位子的好球帶
-export const seatedCoordinate = () => ({
+export const seatedCoordinate = {
   A: {
     3: { leftTop: [3, 8], rightBottom: [16, 20] },
     4: { leftTop: [16, 8], rightBottom: [27, 20] },
@@ -17,4 +18,4 @@ export const seatedCoordinate = () => ({
     2: { leftTop: [26, 9], rightBottom: [35, 20] },
     3: { leftTop: [35, 9], rightBottom: [49, 20] },
   },
-})
+}
