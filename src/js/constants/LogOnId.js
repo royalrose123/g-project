68cc3054ba10f8cee5ebbde5122ab34cb5aac0e1
Royal Chen <royal.chen@cloud-interactive.com>
2019-10-21 16:34:48 +0800

    Update Table Log-on

---
+++ src/js/constants/LogOnId.js
@@ -0,0 +1,12 @@
+export default {
+  // TODO: 未來拿到對應的 Table user, password 再填入
+  'Table-B751': { account: '1', password: '1' },
+  'Table-B752': { account: '2', password: '2' },
+  'Table-B753': { account: '3', password: '3' },
+  'Table-B754': { account: '4', password: '4' },
+  'Table-B755': { account: '5', password: '5' },
+  'Table-B756': { account: '6', password: '6' },
+  'Table-B757': { account: '7', password: '7' },
+  'Table-B758': { account: '8', password: '8' },
+  'Table-0822': { account: '1', password: '1' },
+}
--- src/js/lib/api/Table/denormalizer.js
+++ src/js/lib/api/Table/denormalizer.js
@@ -1,9 +1,12 @@
+import LogOnId from '../../../constants/LogOnId'
 class Denormalizer {
   static LogOnTable ({ tableNumber, account, password }) {
     return {
       tableName: tableNumber,
-      uid: process.env.LOG_ON_ID, // 目前寫死在前端 // Dual PC：'DYFR', CI: '1'
-      upw: process.env.LOG_ON_PASSWORD, // 目前寫死在前端    // Dual PC：'1', CI: '1'
+      // uid: process.env.LOG_ON_ID, // 目前寫死在前端 // Dual PC：'DYFR', CI: '1'
+      // upw: process.env.LOG_ON_PASSWORD, // 目前寫死在前端    // Dual PC：'1', CI: '1'
+      uid: LogOnId[tableNumber]?.account, // TODO: 未來拿到對應的 Table user, password 再填入
+      upw: LogOnId[tableNumber]?.password, // TODO: 未來拿到對應的 Table user, password 再填入
     }
   }
 
